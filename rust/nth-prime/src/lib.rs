3199e477db237735cb09e47fbdd929a473a5aea4
Ezzeri Esa <ezzeri.esa@gmail.com>
2020-08-24 15:06:28 -0700

    up to nth-prime

---
+++ rust/nth-prime/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn nth(n: i32) -> u32 {
+    if n == 0 {
+        return 2
+    }
+
+    let m = n as usize;
+
+    let mut primes = vec![3];
+    let mut recent = 3;
+    let mut count = 1;
+
+    while count < m {
+        recent += 2;
+        let mut is_prime = true;
+
+        for item in &primes {
+            if recent % item == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+
+        if is_prime {
+            primes.push(recent);
+            count += 1;
+        }
+    }
+
+    return primes[m - 1];
+}
+
