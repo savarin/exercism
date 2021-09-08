0d6fe844b1854a26d982dabd8ad120c92c6287ac
Ezzeri Esa <ezzeri.esa@gmail.com>
2021-09-08 14:41:07 -0700

    modify go hamming to ensure same width of characters

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -9,8 +9,8 @@ func Distance(a, b string) (int, error) {
 
 	counter := 0
 
-	for i, char := range a {
-		if char != rune(b[i]) {
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
 			counter += 1
 		}
 	}
